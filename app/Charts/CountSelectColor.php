3b7d24d188ee1e8afedb940f3780381065258473
andypratama3 <andypratama1211@gmail.com>
2023-09-07 16:41:49 +0800

    chart update deleted_at not sum

--- app/Charts/CountSelectColor.php
+++ app/Charts/CountSelectColor.php
@@ -30,7 +30,10 @@ class CountSelectColor
         ->selectRaw('SUM(CASE WHEN pesertas.warna = "abu-abu" THEN 1 ELSE 0 END) as abu_total')
         ->selectRaw('SUM(CASE WHEN pesertas.warna = "merah" THEN 1 ELSE 0 END) as merah_total')
         ->leftJoin('peserta_kecamatan', 'kecamatans.id', '=', 'peserta_kecamatan.kecamatan_id')
-        ->leftJoin('pesertas', 'peserta_kecamatan.peserta_id', '=', 'pesertas.id')
+        ->leftJoin('pesertas', function ($join) {
+            $join->on('peserta_kecamatan.peserta_id', '=', 'pesertas.id')
+                 ->whereNull('pesertas.deleted_at'); // Filter out deleted records
+        })
         ->groupBy('kecamatans.name')
         ->get();
 
